7d8015cf61c3604ddd950ca6b8dd968590b9bf48
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2023-12-21 17:22:21 +0200

    added nvim-tree plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,4 +7,16 @@ Lazy.setup({
     require('plugins.colorscheme'),
     require('plugins.neodev'),
     require('plugins.comment'),
-})
+
+    'tpope/vim-sleuth',                                     -- detects tabstop & shiftwidth automatically
+    'christoomey/vim-tmux-navigator',                       -- vim navigator
+    'szw/vim-maximizer',                                    -- maximise & restore current window
+    { 'folke/which-key.nvim',  opts = {} },                 -- show pending keybinds
+
+    {
+        'lukas-reineke/indent-blankline.nvim',              -- add indentation guides even on blank lines
+        main = "ibl",
+    },
+    
+    require('plugins.nvim-tree'),
+}) 
