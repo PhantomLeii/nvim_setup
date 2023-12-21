9e6b5c37e46ded119d619ce93fde27dd4dfd3d98
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2023-12-21 14:01:55 +0200

    updated init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Clear cache as you load modules
 local load = function (mod)
     package.loaded[mod] = nil
     require(mod)
@@ -5,3 +6,5 @@ end
 
 load('core.options')        -- import user options
 load('core.keymaps')        -- import user keymaps
+
+-- Bootstrap lazy
