7e3b893b946c117cbc2ec5ba29f95563e934a15b
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2023-12-21 13:00:40 +0200

    fixed relativenumber in options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -26,20 +26,21 @@ local opts = {
     swapfile=false,                         -- disallow swap file creation
     timeoutlen=100,                         -- delay for mapped sequences
     undofile=true,                          -- allow persistant undo
-    updatetime=300,                          -- faster completions
+    updatetime=300,                         -- faster completions
     writebackup=false,                      -- disallow swap file editing
     expandtab=true,                         -- convert tabs to spaces
     shiftwidth=2,                           -- spaces for each indentation
     tabstop=2,                              -- insert 2 spaces for tab
     cursorline=true,                        -- highlight focused line
     number=true,                            -- set numbered lines
-    relativenumer=true,                     -- set relative numbered lines
+    relativenumber=true,                    -- set relative numbered lines
     numberwidth=4,                          -- number column width
     signcolumn="yes",                       -- always show sign column
     wrap=true,                              -- wrap long lines
     breakindent=true,                       -- allow smart wrapping (indent wrapped lines)
     scrolloff=8,                            -- leave space while scrolling
-    guifont="JetbrainsMono NF:h17",         -- font used in grafical neovim applicaions
+    guifont="JetbrainsMono:h17",            -- font used in grafical neovim applicaions
+    termguicolors=true,                     -- enable 256 colors
 }
 
 vim.opt.shortmess:append "c"
