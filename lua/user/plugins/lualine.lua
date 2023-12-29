173a59e7213d505e18aa4bf5eba2a49817200e08
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2023-12-29 15:30:11 +0200

    added lualine.lua

---
+++ lua/user/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = { 'diagnostics' },
+        lualine_z = { 'filetype' },
+      },
+      globalstatus = false,
+    }
+  end,
+}
