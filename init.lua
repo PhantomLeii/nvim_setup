c6ea12436eadd035997805f52822f1244b313a4a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2023-12-29 13:01:34 +0200

    deleted old files

--- init.lua
+++
@@ -1,9 +0,0 @@
--- Clear cache as you load modules
-local load = function (mod)
-    package.loaded[mod] = nil
-    require(mod)
-end
-
-load('core.options')        -- import user options
-load('core.keymaps')        -- import user keymaps
-load('plugins')             -- import user plugin configs
